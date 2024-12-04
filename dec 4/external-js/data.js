76693bd3796b0727ba0ea8384fd53995259db79d
gauraayu <gauraayushi2004@gmail.com>
2024-12-04 19:18:01 +0530

    update

---
+++ dec 4/external-js/data.js
@@ -0,0 +1,23 @@
+var a=5;
+// console.log()= cout 
+console.log(a)
+typeof(a)
+// now to check data type we write type of in bracket 
+console.log(typeof(a))    //ans=no.
+
+
+
+
+
+var b="aayu"
+console.log(typeof(b))    //ans=string
+
+
+var c=true;
+console.log(typeof(c))    //boolean
+
+var d;
+console.log(typeof(d))   //undefined this will show error
+
+var e=null;
+console.log(typeof(e))    //object
