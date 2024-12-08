4687d1f98e15af55020cdc6bb349fdd363dbcdc3
gauraayu <gauraayushi2004@gmail.com>
2024-12-08 18:41:47 +0530

    update

--- dec 6/ifelse.js
+++ dec 6/ifelse.js
@@ -2,6 +2,7 @@ function start (){
 
    let ans= window.confirm("like the website")
    //if ok then thanku if cancel then sorry
+   //if elsse ques ussed
    
     if(ans==true){
         alert("thanku")
