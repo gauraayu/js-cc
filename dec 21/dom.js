6e1920b42dc4736cb54873115cdecb79b946f1e2
gauraayu <gauraayushi2004@gmail.com>
2024-12-21 22:43:07 +0530

    upddate

---
+++ dec 21/dom.js
@@ -0,0 +1,15 @@
+let re=()=>{
+
+    let box1=document.querySelector("#box")
+    box1.style.backgroundColor="red"
+}
+let bl=()=>{
+
+    let box1=document.querySelector("#box")
+    box1.style.backgroundColor="red"
+}
+let =()=>{
+
+    let box1=document.querySelector("#box")
+    box1.style.backgroundColor="red"
+}
---
+++ dec 21/image.js
@@ -0,0 +1,15 @@
+let chn1=()=>{
+
+    let box1=document.querySelector("#box")
+    box1.style.backgroundImage="url(https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-16-pro-finish-select-202409-6-9inch-deserttitanium_AV1?wid=5120&hei=2880&fmt=webp&qlt=70&.v=eUdsd0dIb3VUOXdtWkY0VFUwVE8vbEdkZHNlSjBQRklnaFB2d3I5MW94NW9lRVVkRmJ5ZE03VysydEdnMXpSNCtvdWdQMWpIV2NFaHUyTjZvK3ozWmRJVUY1eklwSElSL2pyWFgySnpoc1RLa0ZJQjcvY2pnYVpRdGdHbzJQemdCZk81SjNVTG9zc1RWN0ZXVGFzaFVuNXFNVzc5YkZmK2I4YzJ4ZndkZUdj&traceId=1)"
+}
+let chn2=()=>{
+
+    let box1=document.querySelector("#box")
+    box1.style.backgroundImage="url(https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-16-pro-finish-select-202409-6-9inch-deserttitanium_AV2?wid=5120&hei=2880&fmt=webp&qlt=70&.v=eUdsd0dIb3VUOXdtWkY0VFUwVE8vbEdkZHNlSjBQRklnaFB2d3I5MW94NW9lRVVkRmJ5ZE03VysydEdnMXpSNEN0Mk9PSnZhL2dGRjMvR25CcGJsdDlJVUY1eklwSElSL2pyWFgySnpoc1RLa0ZJQjcvY2pnYVpRdGdHbzJQemdQZ0YzUGN0SVhHczl1WS9uUjcxdmxuNXFNVzc5YkZmK2I4YzJ4ZndkZUdj&traceId=1)"
+}
+let chn3=()=>{
+
+    let box1=document.querySelector("#box")
+    box1.style.backgroundImage="(https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-16-pro-finish-select-202409-6-9inch-deserttitanium_AV3?wid=5120&hei=2880&fmt=webp&qlt=70&.v=eUdsd0dIb3VUOXdtWkY0VFUwVE8vbEdkZHNlSjBQRklnaFB2d3I5MW94NW9lRVVkRmJ5ZE03VysydEdnMXpSNDVST0FtSnQrc0g3M1p4WG5lNzRDb05JVUY1eklwSElSL2pyWFgySnpoc1RLa0ZJQjcvY2pnYVpRdGdHbzJQemdwby95YWVuVDdVVzVEM1gvSzBtN0UzNXFNVzc5YkZmK2I4YzJ4ZndkZUdj&traceId=1)"
+}
