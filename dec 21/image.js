a9700be5959d85391d283a565cdf1b4565485a81
gauraayu <gauraayushi2004@gmail.com>
2024-12-22 23:37:40 +0530

    update

--- dec 21/image.js
+++ dec 21/image.js
@@ -9,7 +9,7 @@ let chn2=()=>{
     box1.style.backgroundImage="url(https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-16-pro-finish-select-202409-6-9inch-deserttitanium_AV2?wid=5120&hei=2880&fmt=webp&qlt=70&.v=eUdsd0dIb3VUOXdtWkY0VFUwVE8vbEdkZHNlSjBQRklnaFB2d3I5MW94NW9lRVVkRmJ5ZE03VysydEdnMXpSNEN0Mk9PSnZhL2dGRjMvR25CcGJsdDlJVUY1eklwSElSL2pyWFgySnpoc1RLa0ZJQjcvY2pnYVpRdGdHbzJQemdQZ0YzUGN0SVhHczl1WS9uUjcxdmxuNXFNVzc5YkZmK2I4YzJ4ZndkZUdj&traceId=1)"
 }
 let chn3=()=>{
-
+// 3rd image
     let box1=document.querySelector("#box")
     box1.style.backgroundImage="(https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-16-pro-finish-select-202409-6-9inch-deserttitanium_AV3?wid=5120&hei=2880&fmt=webp&qlt=70&.v=eUdsd0dIb3VUOXdtWkY0VFUwVE8vbEdkZHNlSjBQRklnaFB2d3I5MW94NW9lRVVkRmJ5ZE03VysydEdnMXpSNDVST0FtSnQrc0g3M1p4WG5lNzRDb05JVUY1eklwSElSL2pyWFgySnpoc1RLa0ZJQjcvY2pnYVpRdGdHbzJQemdwby95YWVuVDdVVzVEM1gvSzBtN0UzNXFNVzc5YkZmK2I4YzJ4ZndkZUdj&traceId=1)"
 }
