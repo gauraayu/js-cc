3d08145f1a949a1ddd0574fb161379b5aedd9648
gauraayu <gauraayushi2004@gmail.com>
2024-12-23 22:28:10 +0530

    update

---
+++ dec 23/dom.js
@@ -0,0 +1,12 @@
+let blueclr=()=>{
+    let box=document.querySelector("#box");
+    box.style.backgroundColor="blue";
+}
+let redclr=()=>{
+    let box=document.querySelector("#box");
+    box.style.backgroundColor="red";
+}
+let whiteclr=()=>{
+    let box=document.querySelector("#box");
+    box.style.backgroundColor="white";
+}
---
+++ dec 23/pure.js
@@ -0,0 +1,17 @@
+// let btn=document.querySelector("#okk");
+// btn.addEventListener("click",()=>{
+//     alert("button clicked")
+
+// })
+// let inp=document.querySelector("#inp")
+// inp.addEventListener("keydown",(E)=>{
+//     console.log(E.key);
+// }
+
+
+// )
+
+let inp=document.querySelector("#inp")
+inp.addEventListener("input",()=>{
+    
+})
